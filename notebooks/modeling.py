624a0705ce78a2b4f0f41991553d8c1bf78a509b
naomidonovan1 <naomi.donovan45@gmail.com>
2025-10-15 15:17:33 -0700

    updated modeling, need to figure out way to store model checkpoints now

--- notebooks/modeling.py
+++ notebooks/modeling.py
@@ -25,17 +25,14 @@ class ConvNet(nn.Module):
         )  # takes input of size (batch, in_channels, height, width) and outputs (batch, 16, height, width)
         self.relu = nn.ReLU()
         self.conv2 = nn.Conv2d(
-            16, 32, kernel_size=3, padding=1
-        )  # outputs (batch, 32, height, width)
+            16, 16, kernel_size=3, padding=1
+        )  # outputs (batch, 16, height, width)
         self.skip = nn.Identity()  # Placeholder for skip connection
-        # self.conv3 = nn.Conv2d(
-        #     48, 64, kernel_size=3, padding=1
-        # )  # expects concatenation of conv1 and conv2 outputs
         self.pool = nn.MaxPool2d(2, 2)
 
         # Task-specific heads
         self.input_size = int(
-            48 * height / 2 * width / 2
+            32 * height / 2 * width / 2
         )  # Assuming input image size is 64x64
         self.grating_head = nn.Linear(self.input_size, 10)  # 10 classes for gratings
         self.natural_scene_head = nn.Linear(
@@ -151,43 +148,30 @@ def add_noise_to_middle_layer(model, noise_level=0.5):
     return model
 
 
-def add_noise_to_last_layer(model, noise_level=0.5):
-    """Injects Gaussian noise ONLY into the final layer."""
-    with torch.no_grad():
-        noise = (
-            torch.randn_like(model.conv3.weight.data) * noise_level
-        )  # returns tensor filled w/ random numbers from standard normal distribution * noise_level
-        model.conv3.weight.data += noise
-    return model
-
-
 def train_model(
-    model, train_loader, gabor_val_loader=None, cifar_val_loader=None, num_epochs=10
+    model,
+    train_loader,
+    combined_val_loader=None,
+    gabor_val_loader=None,
+    cifar_val_loader=None,
+    num_epochs=10,
 ):
     model.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-    losses = []
+    train_losses, val_losses = [], []
     gabor_val_accuracies = []
     cifar_val_accuracies = []
     for epoch in trange(num_epochs, desc="Epochs"):
-        running_loss = 0.0
 
-        if gabor_val_loader:
-            gabor_accuracy = evaluate_gabor_accuracy(model, gabor_val_loader)
-            gabor_val_accuracies.append(gabor_accuracy)
-
-        if cifar_val_loader:
-            cifar_accuracy = evaluate_cifar_accuracy(model, cifar_val_loader)
-            cifar_val_accuracies.append(cifar_accuracy)
-
-        for i, data in tqdm(
+        for _, data in tqdm(
             enumerate(train_loader),
             desc=f"Batches in epoch #{epoch+1}",
             leave=False,
             total=len(train_loader),
         ):
+            running_loss = 0.0
             inputs, labels = data
             inputs, labels = inputs.to(device), labels.to(device)
 
@@ -197,21 +181,66 @@ def train_model(
             is_cifar = labels < 10
             is_gabor = labels >= 10
 
-            loss = 0
+            train_loss = 0
             if is_cifar.any():
-                loss += criterion(out_natural_scene[is_cifar], labels[is_cifar])
+                train_loss += criterion(out_natural_scene[is_cifar], labels[is_cifar])
             if is_gabor.any():
-                loss += criterion(
+                train_loss += criterion(
                     out_grating[is_gabor], labels[is_gabor] - 10
                 )  # adjust labels for gabor
 
-            loss.backward()
+            running_loss += (
+                train_loss.item()
+                if isinstance(train_loss, torch.Tensor)
+                else train_loss
+            )
+            train_losses.append(running_loss)
+
+            running_loss = 0.0
+            if combined_val_loader:
+                with torch.no_grad():
+                    for data in combined_val_loader:
+                        images, labels = data
+                        images, labels = images.to(device), labels.to(device)
+                        out_grating, out_natural_scene = model(
+                            images
+                        )  # Only use combined output
+
+                        is_cifar = labels < 10
+                        is_gabor = labels >= 10
+
+                        val_loss = 0
+                        if is_cifar.any():
+                            val_loss += criterion(
+                                out_natural_scene[is_cifar], labels[is_cifar]
+                            )
+                        if is_gabor.any():
+                            val_loss += criterion(
+                                out_grating[is_gabor], labels[is_gabor] - 10
+                            )  # adjust labels for gabor
+
+                        running_loss += (
+                            val_loss.item()
+                            if isinstance(val_loss, torch.Tensor)
+                            else val_loss
+                        )
+
+                val_losses.append(running_loss / len(combined_val_loader))
+
+            if gabor_val_loader:
+                gabor_accuracy = evaluate_gabor_accuracy(model, gabor_val_loader)
+                gabor_val_accuracies.append(gabor_accuracy)
+
+            if cifar_val_loader:
+                cifar_accuracy = evaluate_cifar_accuracy(model, cifar_val_loader)
+                cifar_val_accuracies.append(cifar_accuracy)
+
+            train_loss.backward()
             optimizer.step()
-            running_loss += loss.item() if isinstance(loss, torch.Tensor) else loss
 
-        losses.append(running_loss / len(train_loader))
+        # train_losses.append(running_loss / len(train_loader))
 
-    return model, losses, gabor_val_accuracies, cifar_val_accuracies
+    return model, train_losses, val_losses, gabor_val_accuracies, cifar_val_accuracies
 
 
 def evaluate_gabor_accuracy(model, gabor_test_loader):
